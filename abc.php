2ae411755a00933aaa5afe093dc1af8ef1dd06b5
Houjifa <hasanrahib113@gmail.com>
2025-08-20 16:50:46 +0600

    phptask

---
+++ abc.php
@@ -0,0 +1,4 @@
+<?php
+print_r($_GET);
+echo "my name is: ".$_GET["name"];
+?>
---
+++ ftask2.php
@@ -0,0 +1,7 @@
+<?php
+$amount =200;
+$vat=0.15*$amount;
+
+echo "vat = $vat <br>";
+echo "Total amount with vat =". ($amount+$vat);
+?>

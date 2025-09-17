7df2cc3bc060c6b86146aa0f0c9df5d67277f6c9
Houjifa113 <138584919+Houjifa113@users.noreply.github.com>
2025-09-17 17:09:33 +0600

    Hehe I am Evan

--- abc.php
+++
@@ -1,4 +0,0 @@
-<?php
-print_r($_GET);
-echo "my name is: ".$_GET["name"];
-?>
--- ftask2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$amount =200;
-$vat=0.15*$amount;
-
-echo "vat = $vat <br>";
-echo "Total amount with vat =". ($amount+$vat);
-?>
